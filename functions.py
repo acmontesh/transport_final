bc7a7899d883ec60ecc4bfc60f017c674f0e8765
scalleriorapid <scallerio@utexas.edu>
2022-11-27 15:17:40 -0500

    Add functions for temp, r and z arrays

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@
 #   SET OF FUNCTIONS TO SOLVE ENERGY EQ. IN FINAL PROJECT TP. ####
 #############  VORTICITY TEAM ####################################
 ##################################################################
-
+import numpy as np
 def vz(r,inside=1):
     # vz is the z-velocity as a function of r in ft/s. 
     #r is the distance from the center of the drill pipe, in inches. 
@@ -36,3 +36,33 @@ def lambda2(r, dr):
     #r is the distance from the center of the drill pipe, in inches. 
     #dr is the cell length in the grid, in ft. 
     return dr*3/r
+
+def comp_temp_ij(vz, r, dr, alpha, t_iplus1, t_iminus1, t_jplus1, t_jminus1):
+    '''
+    Returns Temperature at current index ij.
+    :param vz: fluid velocity in ft/s
+    :param r: distance from the center of the drill pipe, in inches
+    :param dr: cell length in the grid, in ft
+    :param alpha: mud thermal diffusivity in ft^2/s
+    :param t_iplus1: Temperature in i+1, j
+    :param t_iminus1: Temperature in i-1, j
+    :param t_jplus1: Temperature in i, j+1
+    :param t_jminus1: Temperature in i, j-1
+    :return: t_ij (Temperature at current index ij)
+    '''
+
+    # Compute current lambdas
+    lambda1_ij = lambda1(vz, dr, alpha)
+    lambda2_ij = lambda2(r, dr)
+    # Compute current Temperature
+    t_ij = lambda1_ij(t_iplus1-t_iminus1) + 0.5*(t_iplus1+t_iminus1) - lambda2_ij(t_jplus1 - t_jminus1)
+    return t_ij
+
+def r_array(rmax, jcols):
+#     rstep = np.linspace(0, 10, 5)
+#     rstep_array = np.tile(rstep, (4, 1))
+    return np.linspace(0, rmax, jcols)
+
+def z_array(zmax, irows):
+    return np.linspace(0, zmax, irows)
+
