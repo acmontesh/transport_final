60bc78c50759eceb5d0ead071490654b972d0903
scalleriorapid <scallerio@utexas.edu>
2022-11-27 19:40:00 -0500

    Corrected input values for r_array in vz

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ cpHat = 0.3824 # 1600 J/kg.K to BTU/lbm-degF (1 to 0.00024) - From Magdy Abdel H
 # Review unit conversion
 
 # Define well dimensions
-rmax = 3.25 # 12.25/2 # inches
-r_dp = 2.52 # inches
+rmax = 3.25/12 #ft # 12.25/2 # inches
+r_dp = 2.52/12 #ft # inches
 zmax = 2000 # ft
 z_shoe = 1000 # ft
 
@@ -28,11 +28,12 @@ q_left = 0
 
 # Create input arrays
 r_array, dr = r_array(rmax, jcols)
+r_array_inch = r_array*12
 z_array, dz = z_array(zmax, irows)
 pipe_j = search_index(r_array, r_dp) # j index for r = r_dp
 shoe_i = search_index(z_array, z_shoe) # i index for z = z_shoe
 form_temp_array = gen_form_temp_array(temp_grad, t_surf, z_array)
-vz_array = comp_vz_array(r_array, pipe_j, irows, jcols)
+vz_array = comp_vz_array(r_array_inch, pipe_j, irows, jcols) # r input must be in inches to compute velocity
 alpha = thDiffusivity(k_mud, rho, cpHat)
 
 # Initialize Temperature array
@@ -45,7 +46,7 @@ while (max_error>50) and (i < 100):
     # Set boundaries
     temp_array_old = np.copy(temp_array) # for error computing
     temp_array = set_temp_bc(temp_array, form_temp_array, dr, dz, pipe_j, shoe_i, t_surf, q_top, q_right, q0, q_left, k_mud)
-    temp_array = comp_temp_ij(temp_array, vz_array, r_array, dr, alpha, irows)
+    temp_array = comp_temp_ij(temp_array, vz_array, r_array, dr, dz, alpha, irows)
     error_array = np.absolute(temp_array - temp_array_old)
     max_error = error_array.max()
     print(max_error)
